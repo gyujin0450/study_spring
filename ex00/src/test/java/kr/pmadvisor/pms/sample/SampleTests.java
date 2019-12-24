8c4f086abc2cda285c7555f4eb15f458b6f0f250
최규진 <gyujin0450@naver.com>
2019-12-24 22:22:40 +0900

    (p61)logback 적용

---
+++ ex00/src/test/java/kr/pmadvisor/pms/sample/SampleTests.java
@@ -0,0 +1,34 @@
+
+package kr.pmadvisor.pms.sample;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ch.qos.logback.classic.Logger;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class SampleTests {
+  
+  @Autowired
+  private Restaurant restaurant;
+  private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+  @Test
+  public void testExist() {
+    
+    assertNotNull(restaurant);
+    
+    logger.info("{}",restaurant);
+    logger.info("----------------------------------");
+    logger.info("{}",restaurant.getChef());
+    
+  }
+    
+}
