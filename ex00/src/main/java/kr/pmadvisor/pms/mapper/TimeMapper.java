85f6a91b9c58f885a817dfef7a2578af1b6e72a1
최규진 <gyujin0450@naver.com>
2019-12-25 09:03:59 +0900

    (p93~)MyBatis-Spring 연동 처리

--- ex00/src/main/java/kr/pmadvisor/pms/mapper/TimeMapper.java
+++ ex00/src/main/java/kr/pmadvisor/pms/mapper/TimeMapper.java
@@ -5,9 +5,9 @@ import org.apache.ibatis.annotations.Select;
 public interface TimeMapper {
 
 //	@Select("SELECT sysdate FROM dual")		: 오라클
-	@Select("SELECT sysdate() FROM dual") //: MySQL
+	@Select("SELECT sysdate() FROM dual") //: MySQL - (1)어노테이션 이용방법
 	public String getTime();
 
-	public String getTime2();
+	public String getTime2(); // (2) Mapper 이용방법
 
 }
--- ex00/src/test/java/kr/pmadvisor/pms/persistence/TimeMapperTests.java
+++ ex00/src/test/java/kr/pmadvisor/pms/persistence/TimeMapperTests.java
@@ -2,10 +2,12 @@ package kr.pmadvisor.pms.persistence;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import ch.qos.logback.classic.Logger;
 import kr.pmadvisor.pms.mapper.TimeMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -14,18 +16,20 @@ public class TimeMapperTests {
 	
 	@Autowired
 	private TimeMapper timeMapper;
-
+	private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+	
+	
 	@Test
 	public void testGetTime() {
-//		logger.info(timeMapper.getClass().getName());
-//		logger.info(timeMapper.getTime());
+		logger.info(timeMapper.getClass().getName());
+		logger.info(timeMapper.getTime());
 	}
 
 	@Test
 	public void tetGetTime2() {
 
-//		logger.info("getTime2");
-//		logger.info(timeMapper.getTime2());
+		logger.info("getTime2");
+		logger.info(timeMapper.getTime2());
 
 	}
 
